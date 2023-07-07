547980154cb9cb6344548030fbc19d7edd2bd740
babayagaofficial <baba.yaga.df@gmail.com>
2023-07-07 18:26:03 +0100

    output location of blocks on sequence

--- align_snakemake/integerise_plasmids.py
+++ align_snakemake/integerise_plasmids.py
@@ -29,6 +29,15 @@ def get_unimog(interval_tree):
     intervals.append(")")
     return ' '.join(intervals)
 
+def get_blocks(plasmid, interval_tree):
+    data = {"Plasmid":[], "Block_ID":[], "Start":[], "End":[]}
+    for interval in sorted(interval_tree):
+        data["Plasmid"].append(plasmid)
+        data["Block_ID"].append(interval.data)
+        data["Start"].append(interval.begin)
+        data["End"].append(interval.end)
+    blocks = pd.DataFrame(data)
+    return blocks
 
 def get_coordinates(split_line, index):
     start = int(split_line[index])
@@ -39,7 +48,7 @@ def get_coordinates(split_line, index):
     return start, end
 
 
-def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, identity_threshold=80, length_threshold=200) -> Tuple[str, str]:
+def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasmid_1_name, plasmid_2_name, identity_threshold=80, length_threshold=200) -> Tuple[str, str]:
     subprocess.check_call(f"perl -w $(which dnadiff) {plasmid_1} {plasmid_2} -p {prefix} 2>/dev/null", shell=True)
     show_coords_output = subprocess.check_output(f"show-coords -TrcldH -I {identity_threshold} {prefix}.1delta", shell=True).strip().split(b'\n')  # TODO: what about this threshold?
 
@@ -49,6 +58,7 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, identity_
     len_query = -1
     ref_to_block = IntervalTree()
     query_to_block = IntervalTree()
+    data = {"Plasmids":[], "Block_ID":[], "Start":[], "Stop":[]}
     coverage_ref = 0
     coverage_query = 0
     for block, line in enumerate(show_coords_output):
@@ -79,6 +89,8 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, identity_
         populate_interval_tree_with_unmatched_blocks(query_to_block, len_query, len(ref_to_block)+len(query_to_block)+1, length_threshold)
         plasmid_1_unimogs = get_unimog(ref_to_block)
         plasmid_2_unimogs = get_unimog(query_to_block)
+        blocks_ref = get_blocks(ref_to_block)
+        blocks_query = get_blocks(query_to_block)
 
     for extension in [".1coords", ".1delta", ".delta", ".mcoords", ".mdelta", ".qdiff", ".rdiff", ".report", ".snps"]:
         Path(prefix+extension).unlink()
@@ -88,4 +100,4 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, identity_
     else:
         jaccard = coverage_ref/len_ref
 
-    return plasmid_1_unimogs, plasmid_2_unimogs, jaccard
+    return plasmid_1_unimogs, plasmid_2_unimogs, jaccard, blocks_ref, blocks_query
