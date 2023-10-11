a7788874ca4a4241f01e4c791924ccf61e85bfea
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-10-11 15:43:38 +0100

    Adding small test infrastructure

--- dcj_snakemake/glpk_sol_to_gurobi_sol.py
+++ dcj_snakemake/glpk_sol_to_gurobi_sol.py
@@ -1,24 +1,28 @@
-import sys
+def glpk_sol_to_gurobi_sol(input_fh, output_fh):
+    print("# Solution for model obj", file=output_fh)
 
-print("# Solution for model obj")
+    parsing_solution = False
+    for line in input_fh:
+        line = line.strip()
 
-parsing_solution = False
-for line in sys.stdin:
-    line = line.strip()
+        is_objective_line = line.startswith("Objective:")
+        if is_objective_line:
+            obj_value = int(line.split()[3])
+            print(f"# Objective value = {obj_value}", file=output_fh)
 
-    is_objective_line = line.startswith("Objective:")
-    if is_objective_line:
-        obj_value = int(line.split()[3])
-        print(f"# Objective value = {obj_value}")
+        solution_parsing_started = line == "No. Column name       Activity     Lower bound   Upper bound"
+        if solution_parsing_started:
+            parsing_solution = True
 
-    solution_parsing_started = line == "No. Column name       Activity     Lower bound   Upper bound"
-    if solution_parsing_started:
-        parsing_solution = True
+        if parsing_solution:
+            line_split = line.split()
+            try:
+                int(line_split[0])
+            except (ValueError, IndexError):
+                continue
+            print(f"{line_split[1]} {line_split[3]}", file=output_fh)
 
-    if parsing_solution:
-        line_split = line.split()
-        try:
-            int(line_split[0])
-        except (ValueError, IndexError):
-            continue
-        print(f"{line_split[1]} {line_split[3]}")
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(glpk_sol_to_gurobi_sol(sys.stdin, sys.stdout))
