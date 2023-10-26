ddf7f6db009802e6b886b80e807b0e8577b5af9f
babayagaofficial <baba.yaga.df@gmail.com>
2023-10-26 15:06:04 +0100

    fixed bug that occurs if there are indels in the match

--- align_snakemake/integerise_plasmids.py
+++ align_snakemake/integerise_plasmids.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Tuple
 import pandas as pd
 
-def split_overlaps_naive(matches, r, q, length_threshold):
+def split_overlaps_naive(matches, r, q, overlap_threshold):
     data = []
     rstart = f"{r}start"
     rend = f"{r}end"
@@ -13,7 +13,7 @@ def split_overlaps_naive(matches, r, q, length_threshold):
     overlap_bool = False
     for i in range(len(matches)-1):
         overlap = matches[i][rend]-matches[i+1][rstart]
-        if overlap>length_threshold:
+        if overlap>overlap_threshold:
             if matches[i]["qstrand"] == 1:
                 data.append({rstart:matches[i][rstart], rend:matches[i+1][rstart], qstart:matches[i][qstart], qend:matches[i][qend]-overlap, "qstrand":matches[i]["qstrand"]})
                 data.append({rstart:matches[i+1][rstart], rend:matches[i][rend], qstart:matches[i][qend]-overlap, qend:matches[i][qend], "qstrand":matches[i]["qstrand"]})
@@ -39,23 +39,34 @@ def split_overlaps_greedy(og_matches, r, q, length_threshold):
     rend = f"{r}end"
     qstart = f"{q}start"
     qend = f"{q}end"
-    overlap_bool = False
+    indels = f"{r}indels"
     for i in range(len(matches)-1):
         overlap = matches[i][rend]-matches[i+1][rstart]
+        f = lambda x, j: x-matches[j][indels] if matches[j][indels]>0 else x #if there are indels, overlap might be bigger than the length of the query match
         if overlap>length_threshold:
             if matches[i][rend]-matches[i][rstart] >= matches[i+1][rend]-matches[i+1][rstart]: #lhs match bigger than rhs match, so overlap remains in lhs and is removed from rhs match
                 if matches[i+1][rend]>matches[i][rend]: #ensure that this is a true overlap and not containment
+                    overlap = f(overlap,i+1)
                     if matches[i+1]["qstrand"] == 1:
-                        matches[i+1]={rstart:matches[i][rend], rend:matches[i+1][rend], qstart:matches[i+1][qstart]+overlap, qend:matches[i+1][qend], "qstrand":matches[i+1]["qstrand"]}
+                        rlen = matches[i+1][rend] - matches[i][rend]
+                        qlen = matches[i+1][qend] - (matches[i+1][qstart]+overlap)
+                        matches[i+1]={rstart:matches[i][rend], rend:matches[i+1][rend], qstart:matches[i+1][qstart]+overlap, qend:matches[i+1][qend], "qstrand":matches[i+1]["qstrand"], indels:rlen-qlen, f"{q}indels":qlen-rlen}
                     else:
-                        matches[i+1]={rstart:matches[i][rend], rend:matches[i+1][rend], qstart:matches[i+1][qstart], qend:matches[i+1][qend]-overlap, "qstrand":matches[i+1]["qstrand"]}
+                        rlen = matches[i+1][rend] - matches[i][rend]
+                        qlen = matches[i+1][qend]-overlap - matches[i+1][qstart]
+                        matches[i+1]={rstart:matches[i][rend], rend:matches[i+1][rend], qstart:matches[i+1][qstart], qend:matches[i+1][qend]-overlap, "qstrand":matches[i+1]["qstrand"], indels:rlen-qlen, f"{q}indels":qlen-rlen}
                 else:
-                    matches[i+1]={rstart:-1, rend:-1, qstart:-1, qend:-1, "qstrand":0}
+                    matches[i+1]={rstart:-1, rend:-1, qstart:-1, qend:-1, "qstrand":0, "rindel":0, "qindel":0}
             else: #rhs match bigger than lhs match, so overlap remains in rhs and is removed from lhs match
+                overlap = f(overlap,i)
                 if matches[i]["qstrand"] == 1:
-                    matches[i]={rstart:matches[i][rstart], rend:matches[i+1][rstart], qstart:matches[i][qstart], qend:matches[i][qend]-overlap, "qstrand":matches[i]["qstrand"]}
+                    rlen = matches[i+1][rstart] - matches[i][rstart]
+                    qlen = matches[i][qend]-overlap - matches[i][qstart]
+                    matches[i]={rstart:matches[i][rstart], rend:matches[i+1][rstart], qstart:matches[i][qstart], qend:matches[i][qend]-overlap, "qstrand":matches[i]["qstrand"], indels:rlen-qlen, f"{q}indels":qlen-rlen}
                 else:
-                    matches[i]={rstart:matches[i][rstart], rend:matches[i+1][rstart], qstart:matches[i][qstart]+overlap, qend:matches[i][qend], "qstrand":matches[i]["qstrand"]}
+                    rlen = matches[i+1][rstart] - matches[i][rstart]
+                    qlen = matches[i][qend] - (matches[i][qstart]+overlap)
+                    matches[i]={rstart:matches[i][rstart], rend:matches[i+1][rstart], qstart:matches[i][qstart]+overlap, qend:matches[i][qend], "qstrand":matches[i]["qstrand"], indels:rlen-qlen, f"{q}indels":qlen-rlen}
     return matches
 
 def make_interval_tree_w_dups(block_coords):
@@ -78,7 +89,7 @@ def make_interval_tree_w_dups(block_coords):
             query_to_block[qstart:qend] = qstrand * max_id
     return ref_to_block, query_to_block, max_id
 
-def make_interval_tree(block_coords):
+def make_interval_tree(block_coords, length_threshold):
     ref_to_block = IntervalTree()
     query_to_block = IntervalTree()
     max_id = 0
@@ -89,9 +100,10 @@ def make_interval_tree(block_coords):
         qend = block_coords[i]["qend"]
         qstrand = block_coords[i]["qstrand"]
         if not (rstart == rend or qstart == qend):
-            max_id=max_id+1
-            ref_to_block[rstart:rend] = max_id
-            query_to_block[qstart:qend] = qstrand * max_id
+            if rend-rstart>length_threshold or qend-qstart>length_threshold:
+                max_id=max_id+1
+                ref_to_block[rstart:rend] = max_id
+                query_to_block[qstart:qend] = qstrand * max_id
     return ref_to_block, query_to_block, max_id
 
 def populate_interval_tree_with_unmatched_blocks(interval_tree, total_length, block_index, length_threshold):
@@ -168,7 +180,8 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasmid_1
         len_ref = int(split_line[7])
         len_query = int(split_line[8])
         strand_query = int(split_line[12])
-        og_matches.append({"rstart":start_ref, "rend":end_ref, "qstart":start_query, "qend":end_query, "qstrand":strand_query})
+        indels = int(split_line[4]) - int(split_line[5]) #length of ref match - length of qry match, i.e. number of insertions relative to query. if positive, there are insertions in the reference, if negative, then in the query
+        og_matches.append({"rstart":start_ref, "rend":end_ref, "qstart":start_query, "qend":end_query, "qstrand":strand_query, "rindels": indels, "qindels": -indels})
 
 
     coverage_ref = 0
@@ -180,15 +193,16 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasmid_1
         blocks_ref = pd.DataFrame({"Plasmid":[], "Block_ID":[], "Start":[], "End":[]})
         blocks_query = pd.DataFrame({"Plasmid":[], "Block_ID":[], "Start":[], "End":[]})
     else:
+        overlap_threshold = 20
         coverage_ref = get_coverage(ref_coverage)
         coverage_query = get_coverage(query_coverage)
         print(og_matches)
-        ref_split = split_overlaps_greedy(og_matches, "r", "q", length_threshold)
+        ref_split = split_overlaps_greedy(og_matches, "r", "q", overlap_threshold)
         print(ref_split)
         blub = sorted(ref_split, key=lambda match: match["qstart"])
-        block_coords = split_overlaps_greedy(blub, "q", "r", length_threshold)
+        block_coords = split_overlaps_greedy(blub, "q", "r", overlap_threshold)
         print(block_coords)
-        ref_to_block, query_to_block, max_id = make_interval_tree(block_coords)
+        ref_to_block, query_to_block, max_id = make_interval_tree(block_coords, length_threshold)
         populate_interval_tree_with_unmatched_blocks(ref_to_block, len_ref, max_id+1, length_threshold)
         populate_interval_tree_with_unmatched_blocks(query_to_block, len_query, len(ref_to_block)+1, length_threshold)
         plasmid_1_unimogs = get_unimog(ref_to_block)
@@ -212,7 +226,9 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasmid_1
 testing = False
 
 if testing == True:
-    plasmid1 = "NZ_LT985234.1"
-    plasmid2 = "NZ_CP062902.1"
+    #plasmid1 = "NZ_LT985234.1"
+    #plasmid2 = "NZ_CP062902.1"
+    plasmid1 = "NZ_LR999867.1"
+    plasmid2 = "NZ_CP032890.1"
     path = "/home/daria/Documents/projects/INC-plasmids/samples/fastas/incy"
     print(integerise_plasmids(f"{path}/{plasmid1}.fna", f"{path}/{plasmid2}.fna", f"{plasmid1}~{plasmid2}", plasmid1, plasmid2))
