c53f88cd10a0f2ea559f3f02cc9f69626c84d980
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-11-23 16:29:04 +0000

    test: adding test_pling_anno_end_to_end test

--- tests/integration_test/test_integration_test.py
+++ tests/integration_test/test_integration_test.py
@@ -39,6 +39,34 @@ class Test_Pling_end_to_end(TestCase):
         run_pling.pling(args)
 
         assert_files_are_identical("tests/integration_test/data/out_align/all_plasmids_matrix.dist",
-                                   "tests/integration_test/data/all_plasmids_matrix.truth.dist")
+                                   "tests/integration_test/data/all_plasmids_matrix.align.truth.dist")
         assert_files_are_identical("tests/integration_test/data/out_align/all_plasmids_distances.tsv",
-                                   "tests/integration_test/data/all_plasmids_distances.truth.tsv")
+                                   "tests/integration_test/data/all_plasmids_distances.align.truth.tsv")
+
+    def test_pling_anno_end_to_end(self):
+        args = Namespace(genomes_list='tests/integration_test/data/incy_list_4.txt',
+                         output_dir='tests/integration_test/data/out_anno',
+                         integerisation='anno',
+                         bakta_db="tests/integration_test/data/bakta_db",
+                         jaccard_distance=0.6,
+                         dcj=4,
+                         dedup=True,
+                         dedup_threshold=98.5,
+                         identity=80,
+                         min_indel_size=200,
+                         bh_connectivity=10,
+                         bh_neighbours_edge_density=0.2,
+                         small_subcommunity_size_threshold=4,
+                         cores='2',
+                         storetmp=False,
+                         forceall=True,
+                         ilp_solver='GLPK',
+                         timelimit=None,
+                         resources=None,
+                         profile=None)
+        run_pling.pling(args)
+
+        assert_files_are_identical("tests/integration_test/data/out_anno/all_plasmids_matrix.dist",
+                                   "tests/integration_test/data/all_plasmids_matrix.anno.truth.dist")
+        assert_files_are_identical("tests/integration_test/data/out_anno/all_plasmids_distances.tsv",
+                                   "tests/integration_test/data/all_plasmids_distances.anno.truth.tsv")
