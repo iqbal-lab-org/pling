f14385d229165d3b79e3ba177b350156dffbccb6
babayagaofficial <baba.yaga.df@gmail.com>
2023-12-01 18:34:38 +0000

    Now account for indels and containment

--- pling/align_snakemake/matches.py
+++ pling/align_snakemake/matches.py
@@ -10,6 +10,11 @@ class Indel:
         self.len = len
         self.rstart = rstart
         self.qstart = qstart
+        self.rend = rstart+len
+        self.qend = qstart+len
+
+    def __str__(self):
+        return f"({self.rstart}, {self.rend}, {self.qstart}, {self.qend}, {self.type})"
 
 class Match:
     def __init__(self, rstart, rend, qstart, qend, strand, indels=[]):
@@ -41,26 +46,35 @@ class Match:
         return self.rlen()-self.qlen()
 
     def projection(self, coord, ref_bool): #if ref_bool=True, project from reference to query, else query to reference
-        if ref_bool = True:
-            dist = self.rstart - coord
+        if ref_bool:
+            dist = coord - self.rstart
             for indel in self.indels:
-                if indel.rstart<=coord<indel.rstart+indel.len:
+                if indel.rstart<=coord<indel.rend:
                     return indel.qstart
                 elif indel.rstart<coord:
                     if indel.type == "INS":
                         dist += indel.len
                     elif indel.type == "DEL":
                         dist -= indel.len
+            if self.strand == 1:
+                projected_coord = self.qstart + dist
+            else:
+                projected_coord = self.qend - dist
         else:
-            dist = self.qstart - coord
+            dist = coord - self.qstart
             for indel in self.indels:
-                if indel.qstart<=coord<indel.qstart+indel.len:
+                if indel.qstart<=coord<indel.qend:
                     return indel.rstart
                 elif indel.qstart<coord:
                     if indel.type == "INS":
                         dist += indel.len
                     elif indel.type == "DEL":
                         dist -= indel.len
+            if self.strand == 1:
+                projected_coord = self.rstart + dist
+            else:
+                projected_coord = self.rend - dist
+        return projected_coord
 
 class Matches:
     def __init__(self, list_of_matches): #list_of_matches is a list of Match objects
@@ -143,34 +157,37 @@ class Matches:
 
     def split_match(self, match, start, end, ref_bool): #given an interval (start,end) on ref/query genome, split up the match (containing it)
         index = self.list.index(match)
+        projected_start = match.projection(start,ref_bool)
+        projected_end = match.projection(end,ref_bool)
         if ref_bool:
-            lhs_len = start - match.rstart
-            rhs_len = match.rend - end
             if match.strand == 1:
-                lhs_split = Match(match.rstart, start, match.qstart, match.qstart + lhs_len, 1)
-                interval = Match(start, end, match.qstart + lhs_len, match.qend - rhs_len, 1)
-                rhs_split = Match(end, match.rend, match.qend - rhs_len, match.qend, 1)
+                lhs_split = Match(match.rstart, start, match.qstart, projected_start, 1)
+                interval = Match(start, end, projected_start, projected_end, 1)
+                rhs_split = Match(end, match.rend, projected_end, match.qend, 1)
             else:
-                rhs_split = Match(match.rstart, start, match.qend-lhs_len, match.qend, -1 )
-                interval = Match(start, end, match.qstart+rhs_len, match.qend-lhs_len, -1)
-                lhs_split = Match(end, match.rend, match.qstart, match.qstart +rhs_len, -1)
+                rhs_split = Match(match.rstart, start, projected_start, match.qend, -1 )
+                interval = Match(start, end, projected_end, projected_start, -1)
+                lhs_split = Match(end, match.rend, match.qstart, projected_end, -1)
         else:
-            lhs_len = start - match.qstart
-            rhs_len = match.qend - end
             if match.strand == 1:
-                lhs_split = Match(match.rstart, match.rstart + lhs_len, match.qstart, start,  1)
-                interval = Match(match.rstart + lhs_len, match.rend - rhs_len, start, end, 1)
-                rhs_split = Match(match.rend - rhs_len, match.rend, end, match.qend, 1)
+                lhs_split = Match(match.rstart, projected_start, match.qstart, start,  1)
+                interval = Match(projected_start, projected_end, start, end, 1)
+                rhs_split = Match(projected_end, match.rend, end, match.qend, 1)
             else:
-                rhs_split = Match(match.rend-lhs_len, match.rend, match.qstart, start, -1 )
-                interval = Match(match.rstart+rhs_len, match.rend-lhs_len, start, end, -1)
-                lhs_split = Match(match.rstart, match.rstart +rhs_len, end, match.qend, -1)
-        self[index] = lhs_split #add even if null interval bc otherwise will break the walk from left to right -- null interval from here will be removed later
-        self.insert(index+1, interval)
-        if rhs_split.rend != rhs_split.rstart and rhs_split.qend!=rhs_split.qstart: #don't add null interval
-            self.insert(index+2, rhs_split)
+                rhs_split = Match(projected_start, match.rend, match.qstart, start, -1 )
+                interval = Match(projected_end, projected_start, start, end, -1)
+                lhs_split = Match(match.rstart, projected_end, end, match.qend, -1)
+        if lhs_split.rend != lhs_split.rstart or lhs_split.qend!=lhs_split.qstart:
+            self[index] = lhs_split #add even if null interval bc otherwise will break the walk from left to right -- null interval from here will be removed later
+            self.insert(index+1, interval)
+            if rhs_split.rend != rhs_split.rstart or rhs_split.qend!=rhs_split.qstart: #don't add null interval
+                self.insert(index+2, rhs_split)
+        else:
+            self[index] = interval #add even if null interval bc otherwise will break the walk from left to right -- null interval from here will be removed later
+            if rhs_split.rend != rhs_split.rstart or rhs_split.qend!=rhs_split.qstart: #don't add null interval
+                self.insert(index+1, rhs_split)
 
-    def find_opposite_overlaps(self, i, roverlap, ref_bool):
+    def find_opposite_overlaps(self, i, ref_bool):
         overlaps = []
         rstart_1 = self[i].rstart
         rend_1 = self[i].rend
@@ -181,29 +198,35 @@ class Matches:
         qstart_2 = self[i+1].qstart
         qend_2 = self[i+1].qend
         if ref_bool:
-            f = lambda x, j: x-self[j].indels() if self[j].indels()>0 else x #if there are indels, overlap might be bigger than the length of the query match
-            overlap = f(roverlap,i)
+            if rend_1>rend_2: #check for containment
+                rend_1 = self[i+1].rend
+            projected_rstart_2 = self[i].projection(rstart_2, ref_bool)
+            projected_rend_1 = self[i].projection(rend_1, ref_bool)
             if self[i].strand == 1:
-                overlaps.append(Match(rstart_2, rend_1, qend_1-overlap, qend_1, 1))
+                overlaps.append(Match(rstart_2, rend_1, projected_rstart_2, projected_rend_1, 1))
             else:
-                overlaps.append(Match(rstart_2, rend_1, qstart_1, qstart_1+overlap, -1))
-            overlap = f(roverlap,i+1)
+                overlaps.append(Match(rstart_2, rend_1, projected_rend_1, projected_rstart_2, -1))
+            projected_rend_1 = self[i+1].projection(rend_1,ref_bool)
+            projected_rstart_2 = self[i+1].projection(rstart_2, ref_bool)
             if self[i+1].strand == 1:
-                overlaps.append(Match(rstart_2, rend_1, qstart_2, qstart_2+overlap, 1))
+                overlaps.append(Match(rstart_2, rend_1, projected_rstart_2, projected_rend_1, 1))
             else:
-                overlaps.append(Match(rstart_2, rend_1, qend_2-overlap, qend_2, -1))
+                overlaps.append(Match(rstart_2, rend_1, projected_rend_1, projected_rstart_2, -1))
         else:
-            f = lambda x, j: x+self[j].indels() if self[j].indels()<0 else x #if there are indels, overlap might be bigger than the length of the query match
-            overlap = f(roverlap,i)
+            if qend_1>qend_2:
+                qend_1 = self[i+1].qend
+            projected_qstart_2 = self[i].projection(qstart_2, ref_bool)
+            projected_qend_1 = self[i].projection(qend_1, ref_bool)
             if self[i].strand == 1:
-                overlaps.append(Match(rend_1-overlap, rend_1, qstart_2, qend_1, 1))
+                overlaps.append(Match(projected_qstart_2, projected_qend_1, qstart_2, qend_1, 1))
             else:
-                overlaps.append(Match(rstart_1, rstart_1+overlap, qstart_2, qend_1, -1))
-            overlap = f(roverlap,i+1)
+                overlaps.append(Match(projected_qend_1, projected_qstart_2, qstart_2, qend_1, -1))
+            projected_qend_1 = self[i+1].projection(qend_1,ref_bool)
+            projected_qstart_2 = self[i+1].projection(qstart_2, ref_bool)
             if self[i+1].strand == 1:
-                overlaps.append(Match(rstart_2, rstart_2+overlap, qstart_2, qend_1, 1))
+                overlaps.append(Match(projected_qstart_2, projected_qend_1, qstart_2, qend_1, 1))
             else:
-                overlaps.append(Match(rend_2-overlap, rend_2, qstart_2, qend_1, -1))
+                overlaps.append(Match(projected_qend_1, projected_qstart_2, qstart_2, qend_1, -1))
         return overlaps
 
     def resolve_overlaps(self, overlap_threshold):
@@ -221,7 +244,7 @@ class Matches:
                 not_null = False
                 containment = False
             if not_null and overlap>overlap_threshold:
-                overlap_matches = self.find_opposite_overlaps(i, overlap, False)
+                overlap_matches = self.find_opposite_overlaps(i, False)
                 contain_overlap_1 = self.contain_interval(overlap_matches[0].rstart, overlap_matches[0].rend, True)
                 for match in contain_overlap_1:
                     self.split_match(match, overlap_matches[0].rstart, overlap_matches[0].rend, True)
@@ -232,8 +255,8 @@ class Matches:
                     current_match = self[i]
                     self.sort(False)
                     i = self.list.index(current_match)
+                #i = self.list.index(overlap_matches[0]) - 1
             i = i+1
-        self.purge_null_intervals()
         self.sort(True)
         i=0
         finished = False
@@ -242,13 +265,14 @@ class Matches:
             try:
                 overlap = self[i].rend-self[i+1].rstart
                 not_null = self[i].rstart!=self[i].rend and self[i+1].rstart!=self[i+1].rend
+                not_the_same = self[i].rstart!=self[i+1].rstart and self[i].rend!=self[i+1].rend #ignore multicopy
                 containment = self[i+1].rend<self[i].rend
             except IndexError:
                 finished = True
                 not_null = False
                 containment = False
             if not_null and overlap>overlap_threshold:
-                overlap_matches = self.find_opposite_overlaps(i, overlap, True)
+                overlap_matches = self.find_opposite_overlaps(i, True)
                 contain_overlap_1 = self.contain_interval(overlap_matches[0].qstart, overlap_matches[0].qend, False)
                 for match in contain_overlap_1:
                     self.split_match(match, overlap_matches[0].qstart, overlap_matches[0].qend, False)
@@ -292,37 +316,38 @@ def new_integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasm
     indels = []
     len_ref = -1
     len_query = -1
-    for line in show_snps_output:
-        split_line = line.split(b'\t')
-        rsub = split_line[1]
-        qsub = split_line[2]
-        rstart = int(split_line[0])
-        qstart = int(split_line[3])
-        if rsub == ".":
-            type = "INS"
-            try:
-                extend_indel = indels[-1].rstart==rstart and indels[-1].qstart+indels[-1].len=qstart
-            except:
-                extend_indel = False
-            if extend_indel:
-                indels[-1].len += 1
-            else:
-                indels.append(Indel(rstart,qstart,len,type))
-        elif qsub == ".":
-            type = "DEL"
-            try:
-                extend_indel = indels[-1].qstart==qstart and indels[-1].rstart+indels[-1].len=rstart
-            except:
-                extend_indel = False
-            if extend_indel:
-                indels[-1].len += 1
-            else:
-                indels.append(Indel(rstart,qstart,len,type))
-
+    if len(show_snps_output)>1:
+        for line in show_snps_output:
+            split_line = line.split(b'\t')
+            rsub = split_line[1]
+            qsub = split_line[2]
+            rstart = int(split_line[0])
+            qstart = int(split_line[3])
+            if rsub == ".":
+                type = "INS"
+                try:
+                    extend_indel = indels[-1].rstart==rstart and indels[-1].qstart+indels[-1].len==qstart
+                except:
+                    extend_indel = False
+                if extend_indel:
+                    indels[-1].len += 1
+                else:
+                    indels.append(Indel(rstart,qstart,1,type))
+            elif qsub == ".":
+                type = "DEL"
+                try:
+                    extend_indel = indels[-1].qstart==qstart and indels[-1].rstart+indels[-1].len==rstart
+                except:
+                    extend_indel = False
+                if extend_indel:
+                    indels[-1].len += 1
+                else:
+                    indels.append(Indel(rstart,qstart,1,type))
 
     len_ref = -1
     len_query = -1
     og_matches = []
+
     for line in show_coords_output:
         split_line = line.split(b'\t')
 
@@ -335,7 +360,11 @@ def new_integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasm
         len_ref = int(split_line[7])
         len_query = int(split_line[8])
         strand_query = int(split_line[12])
-        og_matches.append(Match(start_ref, end_ref, start_query, end_query, strand_query))
+        match_indels = []
+        for indel in indels:
+            if start_ref<=indel.rstart and end_ref>indel.rend and start_query<=indel.qstart and end_query>indel.qend:
+                match_indels.append(indel)
+        og_matches.append(Match(start_ref, end_ref, start_query, end_query, strand_query, match_indels))
 
     matches = Matches(og_matches)
     coverage_ref = 0
@@ -358,13 +387,13 @@ def new_integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasm
         plasmid_2_unimogs = get_unimog(query_to_block)
         blocks_ref = get_blocks(plasmid_1_name, ref_to_block)
         blocks_query = get_blocks(plasmid_2_name, query_to_block)
-    '''
+
     for extension in [".1coords", ".1delta", ".delta", ".mcoords", ".mdelta", ".qdiff", ".rdiff", ".report", ".snps", ".unqry", ".unref"]:
         try:
             Path(prefix+extension).unlink()
         except:
             pass
-    '''
+
 
     if len_ref>len_query:
         jaccard_similarity = coverage_query/len_query
@@ -378,10 +407,22 @@ testing = True
 if testing == True:
     #plasmid1 = "NZ_LT985234.1"
     #plasmid2 = "NZ_CP062902.1"
-    plasmid2 = "NZ_LR999867.1"
-    plasmid1 = "NZ_CP032890.1"
-    path = "/home/daria/Documents/projects/INC-plasmids/samples/fastas/incy"
-    #plasmid1 = "2_dup_3_dup"
-    #plasmid2 = "3_dup"
-    #path = "/home/daria/Documents/projects/pling/tests/test1/fastas"
+    #plasmid2 = "NZ_LR999867.1"
+    #plasmid1 = "NZ_CP032890.1"
+    #path = "/home/daria/Documents/projects/INC-plasmids/samples/fastas/incy"
+    #plasmid2 = "cpe105_contig_5_np1212"
+    #plasmid1 = "cpe061_contig_3_np1212"
+    plasmid1 = "2_dup_3_dup"
+    plasmid2 = "3_dup"
+    path = "/home/daria/Documents/projects/pling/tests/test1/fastas"
+    #path = "/home/daria/Documents/projects/mobmessing/plasmids_leah"
     print(new_integerise_plasmids(f"{path}/{plasmid1}.fna", f"{path}/{plasmid2}.fna", f"{plasmid1}~{plasmid2}", plasmid1, plasmid2, length_threshold=200))
+    '''matches = Matches([Match(100578,102034,64267,65708,1),Match(101858,101881,57736,57759,1),Match(101881,102188,57759,58867,1)])
+    matches.resolve_overlaps(0)
+    length_threshold=15
+    ref_to_block, query_to_block, max_id = make_interval_tree_w_dups(matches.list, length_threshold)
+    populate_interval_tree_with_unmatched_blocks(ref_to_block, 102034, max_id+1, length_threshold)
+    populate_interval_tree_with_unmatched_blocks(query_to_block, 65708, len(ref_to_block)+1, length_threshold)
+    plasmid_1_unimogs = get_unimog(ref_to_block)
+    plasmid_2_unimogs = get_unimog(query_to_block)
+    print(plasmid_1_unimogs, plasmid_2_unimogs)'''
