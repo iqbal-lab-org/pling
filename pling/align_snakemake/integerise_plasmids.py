20343b86e2f35c39bd9f862306dddb39622e120c
babayagaofficial <baba.yaga.df@gmail.com>
2024-03-01 17:07:55 +0000

    Accidently removed definition of overlap_threshold, added back in.

--- pling/align_snakemake/integerise_plasmids.py
+++ pling/align_snakemake/integerise_plasmids.py
@@ -190,6 +190,7 @@ def integerise_plasmids(plasmid_1: Path, plasmid_2: Path, prefix: str, plasmid_1
     jaccard_distance = 1-jaccard_similarity
 
     if jaccard_distance<=jaccard_threshold:
+        overlap_threshold = 0
         matches.resolve_overlaps(overlap_threshold)
         ref_to_block, query_to_block, max_id = make_interval_tree_w_dups(matches.list, length_threshold)
         populate_interval_tree_with_unmatched_blocks(ref_to_block, len_ref, max_id+1, length_threshold)
