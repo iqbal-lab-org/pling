62d2a52fe2f1b071a64f0104601ce6117ae1705e
babayagaofficial <baba.yaga.df@gmail.com>
2026-05-13 11:21:25 +0200

    (hopefully) more efficient batching when merging clusterings

--- pling/batching/get_batches.py
+++ pling/batching/get_batches.py
@@ -9,9 +9,11 @@ from pling.utils import get_fasta_file_info
 import itertools
 import sys
 
-def get_labels(filepath):
+def get_labels(filepath, paths=False):
     fastafiles, fastaext, fastapath = get_fasta_file_info(filepath)
     genomes = list(fastaext.keys())
+    if paths:
+        return genomes, fastafiles
     return genomes
 
 def append_pair(smash, smash_threshold, smash_matrix, i, j):
@@ -20,64 +22,78 @@ def append_pair(smash, smash_threshold, smash_matrix, i, j):
     else:
         return (1-smash_matrix[i][j]<=smash_threshold)
     
-def previous_pair(genome_1, genome_2, prev_genomes):
-    for genomes in prev_genomes:
-        if genome_1 in genomes and genome_2 in genomes:
-            return True
-    return False
+def write_content(list_a, list_b, index_map, output_dir, batch_size, smash, smash_only, smash_threshold, smash_matrix, contain_file, batch=-1, iter=0):
+    for genome_1, genome_2 in itertools.combinations(list_a, list_b):
+        i = index_map[genome_1]
+        j = index_map[genome_2]
+        append = append_pair(smash, smash_threshold, smash_matrix, i, j)
+        if append:
+            if iter%batch_size==0:
+                if iter!=0:
+                    batch_file.close()
+                batch = batch+1
+                batch_file = open(f"{output_dir}/batch_{batch}.txt","w")
+            batch_file.write(str([genome_1, genome_2])+"\n")
+            iter = iter+1
+            if smash_only:
+                contain_file.write(f"{genome_1}\t{genome_2}\t{1-smash_matrix[i][j]}\n")
+        elif smash:
+            contain_file.write(f"{genome_1}\t{genome_2}\t{1-smash_matrix[i][j]}\n")
+    return contain_file, batch_file, batch, iter
 
-def get_pairs(genomes, batch_size, output_dir, containmentpath, smash, smash_only, smash_matrix = None, smash_threshold = None, prev_genomes = []):
+def get_pairs(genomes, batch_size, output_dir, containmentpath, smash, smash_only, smash_matrix = None, smash_threshold = None, prev_genomes = [], new_genomes=[]):
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
-    n = len(genomes)
-    iter = 0
-    batch = -1
+    index_map = {genome: i for i, genome in enumerate(genomes)}
     if smash or smash_only:
         dir = Path(os.path.dirname(containmentpath))
         dir.mkdir(parents=True, exist_ok=True)
         contain_file = open(containmentpath, "w")
         contain_file.write("plasmid_1\tplasmid_2\tdistance\n")
-    for i,j in itertools.combinations(range(n), 2):
-        if not previous_pair(genomes[i], genomes[j], prev_genomes):
-            append = append_pair(smash, smash_threshold, smash_matrix, i, j)
-            if append:
-                if iter%batch_size==0:
-                    if iter!=0:
-                        batch_file.close()
-                    batch = batch+1
-                    batch_file = open(f"{output_dir}/batch_{batch}.txt","w")
-                batch_file.write(str([genomes[i], genomes[j]])+"\n")
-                iter = iter+1
-                if smash_only:
-                    contain_file.write(f"{genomes[i]}\t{genomes[j]}\t{1-smash_matrix[i][j]}\n")
-            elif smash:
-                contain_file.write(f"{genomes[i]}\t{genomes[j]}\t{1-smash_matrix[i][j]}\n")
+    for list_a, list_b in itertools.combinations(prev_genomes):
+        contain_file, batch_file, batch, iter = write_content(list_a, list_b, index_map, output_dir, batch_size, smash, smash_only, smash_threshold, smash_matrix, contain_file)
+    if prev_genomes == []:
+        new_genomes = genomes
+    if new_genomes!=[]:
+        contain_file, batch_file, batch, iter = write_content(new_genomes, new_genomes, index_map, output_dir, batch_size, smash, smash_only, smash_threshold, smash_matrix, contain_file, batch=batch, iter=iter)
     if smash or smash_only:
         contain_file.close()
     batch_file.close()
     return iter
 
-def containment_file(not_pairs, genome_index, smash_matrix, containmentpath):
-    dir = Path(os.path.dirname(containmentpath))
-    dir.mkdir(parents=True, exist_ok=True)
-    with open(containmentpath, "w") as f:
-        for el in not_pairs:
-            i = genome_index[el[0]]
-            j = genome_index[el[1]]
-            f.write(f"{el[0]}\t{el[1]}\t{1-smash_matrix[i][j]}\n")
+def run_smash_w_prev(genomes, prev_signatures, fastafiles, sig_path, matrixpath):
+    if genomes!=[]:
+        try:
+            paths = " ".join([fastafiles[genome] for genome in genomes])
+            subprocess.run(f"sourmash sketch dna {paths} -o {sig_path}", shell=True, check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(f"ERROR IN RULE: SOURMASH SKETCH FAILING WITH NEW GENOMES", file=sys.stderr)
+            print(e.stderr.decode(), file=sys.stderr)
+            print("END ERROR MSG", file=sys.stderr)
+            raise e
+    else:
+        sig_path = ""
+    try:
+        prev_sigs=" ".join([sig for sig in prev_signatures])
+        subprocess.run(f"sourmash compare {sig_path} {prev_sigs} --max-containment -o {matrixpath}", shell=True, check=True, capture_output=True)
+    except subprocess.CalledProcessError as e:
+        print(f"ERROR IN RULE: SOURMASH COMPARE FAILING", file=sys.stderr)
+        print(e.stderr.decode(), file=sys.stderr)
+        print("END ERROR MSG", file=sys.stderr)
+        raise e
 
 def run_smash(genome_list, sig_path, matrixpath):
     try:
         subprocess.run(f"sourmash sketch dna --from-file {genome_list} -o {sig_path}", shell=True, check=True, capture_output=True)
     except subprocess.CalledProcessError as e:
-        print(f"ERROR IN RULE: SOURMASH FAILING WITH {genome_list}", file=sys.stderr)
+        print(f"ERROR IN RULE: SOURMASH SKETCH FAILING WITH {genome_list}", file=sys.stderr)
         print(e.stderr.decode(), file=sys.stderr)
         print("END ERROR MSG", file=sys.stderr)
         raise e
     try:
         subprocess.run(f"sourmash compare {sig_path} --max-containment -o {matrixpath}", shell=True, check=True, capture_output=True)
     except subprocess.CalledProcessError as e:
-        print(f"ERROR IN RULE: SOURMASH FAILING WITH {genome_list}", file=sys.stderr)
+        print(f"ERROR IN RULE: SOURMASH COMPARE FAILING WITH {genome_list}", file=sys.stderr)
         print(e.stderr.decode(), file=sys.stderr)
         print("END ERROR MSG", file=sys.stderr)
         raise e
@@ -96,24 +112,31 @@ def main():
 
     args = parser.parse_args()
 
+    if args.prev_typing:
+        genomes, fastafiles = get_labels(args.genomes_list, paths=True)
+        prev_genomes = [pd.read_csv(f"{path}/typing.tsv", sep="\t")["plasmid"].to_list() for path in args.prev_typing]
+        prev_hubs = [pd.read_csv(f"{path}/hub_plasmids.csv", sep="\t")["hub_plasmids"].to_list() for path in args.prev_typing]
+        prev_genomes = [prev_genomes[i]+prev_hubs[i] for i in range(len(args.prev_typing))]
+        new_genomes = list(set(genomes)-set(prev_genomes))
+        if args.sourmash or args.sourmash_only:
+                prev_signatures = [f"{path}/sourmash/all_plasmids.sig" for path in args.prev_typing]
+    else:
+        prev_genomes=[]
+
     if args.sourmash or args.sourmash_only:
         sig_dir = Path(f"{args.outputpath}/sourmash")
         sig_dir.mkdir(parents=True, exist_ok=True)
         sig_path = sig_dir/"all_plasmids.sig"
         matrixpath = sig_dir/"smash_containment_matrix"
-        run_smash(args.genomes_list, sig_path, matrixpath)
+        if args.prev_typing:
+            run_smash(new_genomes, prev_signatures, fastafiles, matrixpath, prev_signatures)
+        else:
+            run_smash(args.genomes_list, sig_path, matrixpath)
         genomes = get_labels(f"{matrixpath}.labels.txt")
         smash_matrix = np.load(matrixpath, mmap_mode='r')
     else:
         genomes = get_labels(args.genomes_list)
         smash_matrix = None
-
-    if args.prev_typing:
-        prev_genomes = [pd.read_csv(f"{path}/typing.tsv", sep="\t")["plasmid"].to_list() for path in args.prev_typing]
-        prev_hubs = [pd.read_csv(f"{path}/hub_plasmids.csv", sep="\t")["hub_plasmids"].to_list() for path in args.prev_typing]
-        prev_genomes = [prev_genomes[i]+prev_hubs[i] for i in range(len(args.prev_typing))]
-    else:
-        prev_genomes=[]
 
     len_pairs = get_pairs(genomes, args.batch_size, f"{args.outputpath}/batches", args.containmentpath, args.sourmash, args.sourmash_only, smash_matrix, args.smash_threshold,prev_genomes=prev_genomes)
     number_of_batches = math.ceil(len_pairs/args.batch_size)
